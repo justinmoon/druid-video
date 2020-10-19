d6654ad118d22eadca22d69f4199f6fe1c98dbfe
Justin Moon <mail@justinmoon.com>
2020-10-18 19:00:48 -0500

    Start structuring the background thread

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,65 @@
 mod immediate;
 mod models;
 
-use std::{sync::Arc, thread};
+use std::{
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Arc,
+    },
+    thread,
+};
 
-use druid::widget::{FillStrat, Flex, Image, ImageData, SizedBox};
+use druid::widget::{Button, FillStrat, Flex, Image, ImageData, SizedBox};
 use druid::{
     AppDelegate, AppLauncher, Command, Data, DelegateCtx, Env, ExtEventSink, Lens, LocalizedString,
     Selector, Target, Widget, WidgetExt, WindowDesc,
 };
+use eye::hal::traits::{Device as DeviceTrait, Stream};
+
 use eye::prelude::*;
-use ffimage::packed::dynamic::ImageBuffer;
+use ffimage::packed::dynamic::{ImageBuffer, ImageView};
+
+use crate::models::{Connection, Request, SendWrapper};
 
 const IMAGE_DATA: Selector<ImageData> = Selector::new("IMAGE_DATA");
+const START: Selector<URI> = Selector::new("START");
+const STOP: Selector = Selector::new("STOP");
+
+struct URI(String);
 
 struct Delegate {}
 
 #[derive(Clone, Data, Lens)]
 struct AppState {
+    connection: Arc<models::Connection>,
     buffer: Option<Arc<ImageData>>,
 }
 
 impl AppState {
-    fn new() -> Self {
-        Self { buffer: None }
+    fn new(connection: models::Connection) -> Self {
+        Self {
+            buffer: None,
+            connection: Arc::new(connection),
+        }
     }
 }
 
 // Grab the first device
-fn get_uri() -> Option<String> {
+fn get_uri() -> Option<URI> {
     let devices: Vec<models::Device> = eye::device::Device::enumerate()
         .iter()
         .map(|dev| models::Device::from(dev.as_str()))
         .collect();
 
     if devices.len() > 0 {
-        return Some(devices[0].uri.clone());
+        return Some(URI(devices[0].uri.clone()));
     }
     return None;
 }
 
-fn stream_webcam(sink: ExtEventSink, uri: String) {
+fn open_device(uri: &URI) -> Box<dyn DeviceTrait> {
     // Grab the device
-    let mut device = eye::device::Device::with_uri(&uri).expect("with_uri() error");
+    let mut device = eye::device::Device::with_uri(&uri.0).expect("with_uri() error");
 
     // Set the format
     let mut format = device.format().expect("couldn't read format");
@@ -51,28 +69,80 @@ fn stream_webcam(sink: ExtEventSink, uri: String) {
         panic!("device does not support BGRA capture",);
     }
 
-    // Stream video from webcam
-    let mut stream = device.stream().expect("stream() error");
-    loop {
-        // Grab next webcam frame
-        let image_view = stream.next().expect("failed to get frame");
+    return device;
+}
 
-        // Convert to druid's ImageData
-        // FIXME: can we reduce the amount of conversions here?
-        let ffi_image_buffer = ImageBuffer::from(&*image_view);
-        let image_buffer: image::ImageBuffer<image::Bgra<u8>, Vec<u8>> =
-            image::ImageBuffer::from_raw(
-                ffi_image_buffer.width(),
-                ffi_image_buffer.height(),
-                ffi_image_buffer.raw().as_slice().unwrap().to_vec(),
-            )
-            .expect("Failed to convert ffimage::ImageBuffer to image::ImageBuffer");
-        let dynamic_image = image::DynamicImage::ImageBgra8(image_buffer);
-        let image_data = ImageData::from_dynamic_image_with_alpha(dynamic_image);
+//enum CamThread<'a> {
+//Idle {
+//sink: ExtEventSink,
+//receiver: Receiver<Request>,
+////device: SendWrapper<Box<dyn Device>>,
+//},
+//Streaming {
+//sink: ExtEventSink,
+//receiver: Receiver<Request>,
+//device: SendWrapper<Box<dyn DeviceTrait>>,
+//stream: SendWrapper<Box<dyn 'a + for<'b> Stream<'b, Item = ImageView<'b>>>>,
+//},
+//}
 
-        // Send to render thread
-        sink.submit_command(IMAGE_DATA, image_data, None)
-            .expect("failed to submit command");
+struct MyThread<'a> {
+    sink: ExtEventSink,
+    receiver: Receiver<Request>,
+    device: Option<SendWrapper<Box<dyn DeviceTrait>>>,
+    stream: Option<SendWrapper<Box<dyn 'a + for<'b> Stream<'b, Item = ImageView<'b>>>>>,
+}
+
+impl<'a> MyThread<'a> {
+    fn new(sink: ExtEventSink, receiver: Receiver<Request>) -> Self {
+        Self {
+            sink,
+            receiver,
+            device: None,
+            stream: None,
+        }
+    }
+
+    fn run(&mut self) {
+        loop {
+            // handle next frame
+            if let Some(stream) = &mut self.stream {
+                // Grab next webcam frame
+                let image_view = stream.next().expect("failed to get frame");
+
+                // Convert to druid's ImageData
+                // FIXME: can we reduce the amount of conversions here?
+                let ffi_image_buffer = ImageBuffer::from(&*image_view);
+                let image_buffer: image::ImageBuffer<image::Bgra<u8>, Vec<u8>> =
+                    image::ImageBuffer::from_raw(
+                        ffi_image_buffer.width(),
+                        ffi_image_buffer.height(),
+                        ffi_image_buffer.raw().as_slice().unwrap().to_vec(),
+                    )
+                    .expect("Failed to convert ffimage::ImageBuffer to image::ImageBuffer");
+                let dynamic_image = image::DynamicImage::ImageBgra8(image_buffer);
+                let image_data = ImageData::from_dynamic_image_with_alpha(dynamic_image);
+
+                // Send to render thread
+                self.sink
+                    .submit_command(IMAGE_DATA, image_data, None)
+                    .expect("failed to submit command");
+            }
+
+            // calculate recv timeout
+            let timeout = match self.stream {
+                Some(_) => std::time::Duration::from_millis(0),
+                // iterate more slowly if we're not streaming
+                None => std::time::Duration::from_millis(100),
+            };
+
+            // receive requests
+            if let Ok(request) = self.receiver.recv_timeout(timeout) {
+                match request {
+                    _ => println!("ignored request"),
+                }
+            }
+        }
     }
 }
 
@@ -88,6 +158,10 @@ impl AppDelegate<AppState> for Delegate {
         if let Some(image_data) = cmd.get(IMAGE_DATA) {
             data.buffer = Some(Arc::new(image_data.clone()));
         }
+
+        // stop: remove current frame, send stop message to thread
+
+        // start: send start message to thread
         true
     }
 }
@@ -105,9 +179,13 @@ fn ui_builder() -> impl Widget<AppState> {
         Some(component)
     });
 
+    let start = Button::new("Start").on_click(|_event, data: &mut AppState, _env| {
+        data.connection.start_stream();
+    });
+
     let wrapped_image = SizedBox::new(image).width(200.).height(200.);
 
-    Flex::column().with_child(wrapped_image)
+    Flex::column().with_child(wrapped_image).with_child(start)
 }
 fn main() {
     // Create app instance (we need an event sink)
@@ -115,14 +193,15 @@ fn main() {
     let app = AppLauncher::with_window(main_window);
 
     // Launch webcam thread
-    let uri = get_uri().expect("Couldn't get URI");
+    //let uri = get_uri().expect("Couldn't get URI");
     let sink = app.get_external_handle();
-    thread::spawn(move || {
-        stream_webcam(sink, uri);
-    });
+    let (sender, receiver) = channel();
+
+    thread::spawn(move || MyThread::new(sink, receiver).run());
 
     // Launch app
-    let state = AppState::new();
+    let connection = models::Connection::new(sender);
+    let state = AppState::new(connection);
     let delegate = Delegate {};
     app.delegate(delegate)
         .use_simple_logger()
--- src/models.rs
+++ src/models.rs
@@ -97,3 +97,77 @@ impl<T> DerefMut for SendWrapper<T> {
 }
 
 unsafe impl<T> Send for SendWrapper<T> {}
+
+use std::{io, sync::mpsc};
+
+#[derive(Debug, Clone)]
+pub struct Connection {
+    connection: mpsc::Sender<Request>,
+}
+
+impl Drop for Connection {
+    fn drop(&mut self) {
+        self.stop_stream();
+    }
+}
+
+impl Connection {
+    pub fn new(connection: mpsc::Sender<Request>) -> Self {
+        Connection { connection }
+    }
+
+    pub fn start_stream(&self) {
+        self.connection.send(Request::StartStream).unwrap();
+    }
+
+    pub fn stop_stream(&self) {
+        self.connection.send(Request::StopStream).unwrap();
+    }
+
+    pub fn query_formats(&self) {
+        self.connection.send(Request::QueryFormats).unwrap();
+    }
+
+    pub fn query_controls(&self) {
+        self.connection.send(Request::QueryControls).unwrap();
+    }
+
+    pub fn format(&self) {
+        self.connection.send(Request::GetFormat).unwrap();
+    }
+
+    pub fn set_format(&self, fmt: &Format) {
+        self.connection
+            .send(Request::SetFormat(fmt.clone()))
+            .unwrap();
+    }
+
+    pub fn set_control(&self, ctrl: &Control) {
+        self.connection
+            .send(Request::SetControl(ctrl.clone()))
+            .unwrap();
+    }
+}
+
+#[derive(Debug)]
+pub enum Request {
+    StartStream,
+    StopStream,
+    QueryFormats,
+    QueryControls,
+    GetFormat,
+    SetFormat(Format),
+    SetControl(Control),
+}
+
+// FIXME: I don't think this will get used. We'll have druid commands instead.
+#[derive(Debug)]
+pub enum Response {
+    StartStream(io::Result<()>),
+    StopStream(io::Result<()>),
+    QueryFormats(io::Result<Vec<Format>>),
+    QueryControls(io::Result<Vec<Control>>),
+    GetFormat(io::Result<Format>),
+    SetFormat(io::Result<Format>),
+    SetControl(io::Result<Control>),
+}
